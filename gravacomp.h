1ad15230a115c164aaef39c908ef2c959a286eba
Igor Lemos <j202shyf@z5.anonaddy.me>
2025-04-14 01:38:16 -0300

    Update gravacomp.h

--- gravacomp.h
+++ gravacomp.h
@@ -1,4 +1,9 @@
+#ifndef GRAVACOMP_H
+#define GRAVACOMP_H
+
 #include <stdio.h>
 
-int gravacomp (int nstructs, void* valores, char* campos, FILE* arquivo);
-void mostracomp (FILE* arquivo);
+int gravacomp(int nstructs, void* valores, char* descritor, FILE* arquivo);
+void mostracomp(FILE *arquivo);
+
+#endif
