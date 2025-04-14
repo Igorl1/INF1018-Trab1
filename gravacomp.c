8219594bb296cd2538d8f826523a8ab0c1b62673
Igor Lemos <j202shyf@z5.anonaddy.me>
2025-04-14 19:19:51 -0300

    Initial commit

---
+++ gravacomp.c
@@ -0,0 +1,26 @@
+/* Igor Soares Lemos 2011287 3WC */
+/* Nome_do_Aluno2 Matricula Turma */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include "gravacomp.h"
+
+// Escreve um valor em big endian no arquivo
+void escrever_big_endian(FILE *arquivo, uint32_t val, int nbytes) {
+
+}
+
+// Lê valor big endian
+uint32_t ler_big_endian(FILE *arquivo, int nbytes) {
+    return 0;
+}
+
+int gravacomp(int nstructs, void* valores, char* descritor, FILE* arquivo) {
+    return 0;
+}
+
+void mostracomp(FILE* arquivo) {
+
+}
