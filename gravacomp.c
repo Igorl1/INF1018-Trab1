a503823d758ce953a2be36796339c2a3c4c16824
BrunoCardeman <167805790+BrunoCardeman@users.noreply.github.com>
2025-04-15 11:40:02 -0300

    Update gravacomp.c
    Adicionei o codigo que separa os tipo Exemplo: iis03us10
    - i
    - i
    - s03
    -u 
    -s10

--- gravacomp.c
+++ gravacomp.c
@@ -18,6 +18,32 @@ uint32_t ler_big_endian(FILE *arquivo, int nbytes) {
 }
 
 int gravacomp(int nstructs, void* valores, char* descritor, FILE* arquivo) {
+       int len = strlen(descritor);
+    char* type[10]; // até 10 elementos
+    int cont = -1;
+
+    for (int i = 0; i < len; i++) {
+        if (isalpha(descritor[i])) {
+            // nova letra = novo tipo
+            cont++;
+            type[cont] = (char*)malloc(10 * sizeof(char)); // tamanho suficiente
+            type[cont][0] = descritor[i];
+            type[cont][1] = '\0';
+        } else if (isdigit(descritor[i]) && cont >= 0) {
+            // número = concatena ao último tipo
+            int n = strlen(type[cont]);
+            type[cont][n] = descritor[i];
+            type[cont][n + 1] = '\0';
+        }
+    }
+
+    // printa resultado
+    printf("Resultado:\n");
+    for (int i = 0; i <= cont; i++) {
+        printf("type[%d] = %s\n", i, type[i]);
+        free(type[i]);
+    }
+
     return 0;
 }
 
